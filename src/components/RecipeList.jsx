5700fc82239878c82dc22ba03677b526137df6e3
Ali N. Abadian <nagshrizali@gmail.com>
2024-04-02 19:15:25 +0330

    1

---
+++ src/components/RecipeList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './RecipeList.css'
+
+const RecipeList = () => {
+    return (
+        <div>RecipeList</div>
+    )
+}
+
+export default RecipeList
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import RecipeList from '../../components/RecipeList.jsx'
+import { useFetch } from '../../hooks/useFetch.jsx'
 import './Home.css'
 
 const Home = () => {
+
+    const { data, isLoading, error } = useFetch('http://localhost:3000/recipes')
+
     return (
-        <div>Home</div>
+        <div className='home'>
+            {error && <p className='error'>{error}</p>}
+            {isLoading && <p className='loading'>Loading...</p>}
+            {data && <RecipeList />}
+        </div>
     )
 }
 
