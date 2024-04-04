4d5b436dff68d595bc8532faa0ec582034592811
Ali N. Abadian <nagshrizali@gmail.com>
2024-04-04 21:01:33 +0330

    5

--- src/pages/Recipe/Recipe.jsx
+++ src/pages/Recipe/Recipe.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch.jsx'
+
 import './Recipe.css'
 
 const Recipe = () => {
+    const { id } = useParams()
+    const url = 'http://localhost:3000/recipes/' + id
+    const { data: recipe, isLoading, error } = useFetch(url)
     return (
-        <div>Recipe</div>
+        <div className='recipe'>
+            {error && <p className='error'>{error}</p>}
+            {isLoading && <p className='loading'>Loading...</p>}
+            {recipe && (
+                <>
+                    <h2 className='page-title'>{recipe.title}</h2>
+                    <p>Takes {recipe.cookingTime} to Cook.</p>
+                    <ul>
+                        {recipe.ingredients.map((ing, index) => <li key={index}>{ing}</li>)}
+                    </ul>
+                    <p className='method'>{recipe.method}</p>
+                </>
+            )}
+        </div>
     )
 }
 
