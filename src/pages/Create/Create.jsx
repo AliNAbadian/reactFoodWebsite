e9171efe7f79c604c859dcedbdc899ab730b12e5
Ali N. Abadian <nagshrizali@gmail.com>
2024-04-04 21:13:28 +0330

    Create page

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Create.css'
 
 const Create = () => {
+
+    const [title, setTitle] = useState('')
+    const [method, setMethod] = useState('')
+    const [cookingTime, setCookingTime] = useState(null)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(title, method, cookingTime)
+    }
+
     return (
-        <div>Create</div>
+        <div className='create'>
+            <h2 className='page-title'>Add a New Recipe</h2>
+
+            <form onSubmit={handleSubmit}>
+
+                <label>
+                    <span>Recipe Title: </span>
+                    <input
+                        type="text"
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                        required
+                    />
+                </label>
+
+                <label>
+                    <span>Recipe Method: </span>
+                    <textarea
+                        onChange={(e) => setMethod(e.target.value)}
+                        value={method}
+                        required
+                    />
+                </label>
+
+                <label>
+                    <span>Cooking Time(m): </span>
+                    <input
+                        type="number"
+                        onChange={(e) => setCookingTime(e.target.value)}
+                        value={cookingTime}
+                        required
+                    />
+                </label>
+
+                <button className="btn">Submit</button>
+            </form>
+        </div>
     )
 }
 
